6f32a8ab1bf4f02ca46bad985ef975afcfb28a5a
Grom <arsen.gromov.s@gmail.com>
2021-08-29 03:20:47 +0300

    v0.0006

---
+++ while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	int y;
+	int r;
+
+	x = 1;
+	while (x < 4) {
+	y = 1;
+	
+	while (y < 4) {
+		r = 0;
+		printf (" %d", r = x * y);
+		y++;
+		}		
+	printf("\n");
+	x++;
+	}
+return 0;
+}
